591d2bc35a3f4b510225e64c205f4d81bf32fc72
Victor Buzetto <victoraugustobuzetto@gmail.com>
2026-05-30 09:45:05 -0300

    Refatorando e criando os ultimos casos de testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,16 +1,17 @@
 describe("Funcionalidade: Tela de Login do Portal do teste técnico", () => {
+  const company = Cypress.env("User_Company");
+  const username = Cypress.env("User_Username");
+  const password = Cypress.env("User_Password");
+
   beforeEach(() => {
     cy.start(Cypress.env("URL"));
   });
 
   it("Cenário 01 - Deve realizar o login com as credenciais vállidas e acessar o portal", () => {
-    const company = Cypress.env("User_Company");
-    const username = Cypress.env("User_Username");
-    const password = Cypress.env("User_Password");
-
     cy.loginPortalRDV(company, username, password);
+    cy.submitForm().click();
 
-    cy.url().should(
+    cy.url({ timeout: 20000 }).should(
       "eq",
       "https://portalrdvqa.azurewebsites.net/#/tenant/dashboard",
     );
@@ -18,54 +19,114 @@ describe("Funcionalidade: Tela de Login do Portal do teste técnico", () => {
   });
 
   it("Cenário 02 - Tentativa de login com uma empresa inválida", () => {
-    const username = Cypress.env("User_Username");
-    const password = Cypress.env("User_Password");
-
     cy.loginPortalRDV("Teste", username, password);
+    cy.submitForm().click();
 
-    cy.get(".sweet-alert").should("be.visible");
-    cy.get(".sweet-alert").contains("Falha no login!");
+    cy.alert().should("be.visible");
+    cy.alert().contains("Falha no login!");
   });
 
   it("Cenário 02.1 - Tentativa de login com um usuário incorreto", () => {
-    const company = Cypress.env("User_Company");
-    const password = Cypress.env("User_Password");
-
     cy.loginPortalRDV(company, "12345", password);
+    cy.submitForm().click();
 
-    cy.get(".sweet-alert").should("be.visible");
-    cy.get(".sweet-alert").contains("Falha no login!");
+    cy.alert().should("be.visible");
+    cy.alert().contains("Falha no login!");
   });
 
   it("Cenário 02.2 - Tentativa de login com uma senha incorreta.", () => {
-    const company = Cypress.env("User_Company");
-    const username = Cypress.env("User_Username");
-
     cy.loginPortalRDV(company, username, "senha_senha");
+    cy.submitForm().click();
 
-    cy.get(".sweet-alert").should("be.visible");
-    cy.get(".sweet-alert").contains("Falha no login!");
+    cy.alert().should("be.visible");
+    cy.alert().contains("Falha no login!");
   });
 
-  it("Cenário 03 - Tentativa de login com os campos em branco.", () => {
+  it("Cenário 03 - Tentativa de login com o campo Empresa em branco.", () => {
+    cy.loginPortalRDV("", username, password);
+    cy.submitForm().click();
 
-    cy.get('button[type="submit"]').click()
-  
-    cy.get(".sweet-alert").should("be.visible");
-    cy.get(".sweet-alert").contains("Preencha os campos obrigatórios");
+    cy.alert().should("be.visible");
+  });
+
+  it("Cenário 03.1 - Tentativa de login com o campo Usuário em branco.", () => {
+    cy.loginPortalRDV(company, "", password);
+    cy.submitForm().click();
+
+    cy.alert().should("be.visible");
+  });
+
+  it("Cenário 03.2 - Tentativa de login com o campo Senha em branco.", () => {
+    cy.loginPortalRDV(company, username, "");
+    cy.submitForm().click();
+
+    cy.alert().should("be.visible");
   });
 
   it("Cenário 04 - Tentiva de redefinição de senha.", () => {
-    cy.redefinirSenha(Cypress.env("User_Company"), Cypress.env("User_Username"))
-  
-    cy.get(".sweet-alert").should("be.visible");
-    cy.get(".sweet-alert").contains("Email enviado");
+    cy.redefinirSenha(company, username);
+
+    cy.alert().should("be.visible");
+    cy.alert().contains("Email enviado");
   });
 
-  it.only("Cenário 06 - Tentativa de tradução para o idioma desejado", () => {
+  it("Cenário 04.1 - Tentiva de redefinição de senha deixando o campo Empresa em branco e preenchendo o campo Usuário", () => {
+    cy.redefinirSenha("", username);
+
+    cy.alert().should("be.visible");
+    cy.alert().contains("Informe empresa.");
+  });
+
+  it("Cenário 04.2 - Tentiva de redefinição de senha preenchendo o campo Empresa corretamente e deixando o campo Usuário em branco", () => {
+    cy.redefinirSenha(company, "");
+
+    cy.alert().should("be.visible");
+    cy.alert().contains("Informe usuário.");
+  });
+
+  it("Cenário 04.3 - Tentiva de redefinição de senha inserindo uma Empresa inválida no campo Empresa", () => {
+    cy.redefinirSenha("TESTE", username);
+
+    cy.alert().should("be.visible");
+    cy.alert().contains("Usuário deletado ou não encontrado.");
+  });
+
+  it("Cenário 05 - Tentiva de login com as credenciais corretas e deixar marcado o botão Lembrar", () => {
+    cy.loginPortalRDV(company, username, password);
+    cy.rememberMe().check();
+    cy.submitForm().click();
+
+    cy.url({ timeout: 20000 }).should(
+      "eq",
+      "https://portalrdvqa.azurewebsites.net/#/tenant/dashboard",
+    );
+    cy.get(".page-logo", { timeout: 10000 }).should("be.visible");
+  });
+
+  it("Cenário 05.1 - Tentativa de não armazenar os dados preenchidos após desmarcar a opção lembrar", () => {
+    cy.loginPortalRDV(company, username, password);
+    cy.rememberMe().check();
+    cy.submitForm().click();
+
+    cy.url({ timeout: 20000 }).should(
+      "eq",
+      "https://portalrdvqa.azurewebsites.net/#/tenant/dashboard",
+    );
+    cy.get(".page-logo", { timeout: 10000 }).should("be.visible");
+
+    cy.logout().click({ force: true });
+    cy.get('#tenancyname').should('have.value', '');
+  });
+
+  it("Cenário 06 - Tentativa de tradução para o idioma em Espanhol", () => {
     cy.get('span[title="Espanhol"]').click();
-  
-    cy.get('input[placeholder="Contraseña"]').should('be.visible');
-    cy.get('button[type="submit"]').contains('Iniciar sesión');
+
+    cy.verificarIdioma('Empresa', 'Usuario', 'Contraseña')
+  });
+
+  it("Cenário 06.1 - Tentativa de tradução para o idioma em Inglês", () => {
+    cy.get('span[title="English"]').click();
+
+    cy.verificarIdioma('Company', 'User', 'Password')
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,19 +1,52 @@
-//Para ser usado em todo BeforeEach
+//Utiliztários
 Cypress.Commands.add ('start', (url) => {
     cy.visit(url)
     cy.get('.logo').should('be.visible')
 })
 
+Cypress.Commands.add('alert', () => {
+    cy.get(".sweet-alert")
+})
+
+Cypress.Commands.add('rememberMe', () => {
+    cy.get('input[type="checkbox"]')
+})
+
+Cypress.Commands.add('submitForm', () => {
+    cy.get('button[type="submit"]')
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.get('a[href="/Account/Logout"]')
+})
+
+
 //Login
 Cypress.Commands.add('loginPortalRDV', (company, username, password) => {
+    if (company){
     cy.get('#tenancyname').type(company)
+    }
+    if (username){
     cy.get('#usuario').type(username)
+    }
+    if (password){
     cy.get('#passwordLogin').type(password, {log: false })
-    cy.get('button[type="submit"]').click()
+    }
 });
 
 Cypress.Commands.add('redefinirSenha', (company, username, ) => {
+    if (company){
     cy.get('#tenancyname').type(company)
+    }
+    if (username){
     cy.get('#usuario').type(username)
+    }
     cy.get('#forget-passwordlink').click()
 });
+
+Cypress.Commands.add('verificarIdioma', (placeCompany, placeUser, placePassword) => {
+    cy.get('#tenancyname').should('have.attr', 'placeholder', placeCompany);
+    cy.get('#usuario').should('have.attr', 'placeholder', placeUser);
+    cy.get('#passwordLogin').should('have.attr', 'placeholder', placePassword);
+    
+})
